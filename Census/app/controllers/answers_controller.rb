8d34b5c764a160d80610745b3e9ac5c62bbc334e
khushnaseeb <khushnaseeb14@yahoo.co.in>
2013-07-02 11:21:41 +0530

    successfully donegit commit

--- Census/app/controllers/answers_controller.rb
+++ Census/app/controllers/answers_controller.rb
@@ -9,10 +9,10 @@ class AnswersController < ApplicationController
   end
 
   def create
-
-    p = params[:option_selected]
-    if p!=nil
+   p = params[:option_selected]
+    if p!=nil 
       params[:option_selected].each_pair {|key, value| @answer=Answer.create(user_id:current_user.id,option_id:value,question_id:key,survey_id:Question.find(key).survey_id)}    
+      
       redirect_to users_path
     else
       render  "users/show"
--- Census/app/controllers/users_controller.rb
+++ Census/app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
 
   def index
+    if current_user.answers.all.size==0
+      @message="you have not given any surveys!!"
+      redirect_to surveys_path(@message)
+    end
   end
 
   def show
-    @survey=Survey.find(params[:id])
-    #current_user.survey_id=params[:id]
+    if current_user.answers.where(:survey_id=>params[:id]).empty?
+      p Answer.where(params[:id])
+      @survey=Survey.find(params[:id])
+       #current_user.survey_id=params[:id]
+    else
+      redirect_to surveys_path
+    end
   end
 
 end
